409ea952ce237ab6691ed8e9af637a98fcf04150
EdwardFordan <132804180+fordanedward@users.noreply.github.com>
2025-08-29 23:53:00 +0800

    time slot updated

--- src/routes/auth/appointment/+page.svelte
+++ src/routes/auth/appointment/+page.svelte
@@ -29,10 +29,10 @@
   const stripePromise = loadStripe(env.stripe.publicKey);
 
   const ALL_POSSIBLE_MORNING_SLOTS = [
-      "9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:30 AM", "12:00 PM", "12:30 PM",
+      "8:00 AM", "8:30 AM", "9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:30 AM", "12:00 PM", "12:30 PM",
   ];
   const ALL_POSSIBLE_AFTERNOON_SLOTS = [
-      "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM",
+      "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM",
   ];
   const ALL_POSSIBLE_SLOTS = [...ALL_POSSIBLE_MORNING_SLOTS, ...ALL_POSSIBLE_AFTERNOON_SLOTS];
 
