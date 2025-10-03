e659ce0a076993436957221669e135aa084b0459
fordanedward <202210106@gordoncollege.edu.ph>
2025-10-03 18:43:37 +0800

    updated landing

--- src/routes/registerPatient/+page.svelte
+++ src/routes/registerPatient/+page.svelte
@@ -494,7 +494,7 @@
                     class="w-full p-2 border border-gray-300 text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300 flex items-center justify-center"
                 >
                     <GoogleSolid class="w-5 h-5 mr-2" />
-                    {isGoogleSigningIn ? 'Connecting...' : 'Sign up with Google'}
+                    {isGoogleSigningIn ? 'Connecting...' : 'Register with Google'}
                 </Button>
             </div>
 
